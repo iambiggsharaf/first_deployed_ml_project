7850b58a7525b43b22839123005ba0c707c1c895
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-07-23 19:32:06 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,19 +16,6 @@ df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/pe
 st.subheader("📋 Случайные 10 строк")
 st.dataframe(df.sample(10), use_container_width=True)
 
-st.subheader("📊 Визуализация данных")
-col1, col2 = st.columns(2)
-with col1:
-    fig1 = px.histogram(df, x="species", color="island", barmode="group", title="Распределение видов по островам")
-    st.plotly_chart(fig1, use_container_width=True)
-with col2:
-    fig2 = px.scatter(df, x="bill_length_mm", y="flipper_length_mm", color="species", title="Длина клюва vs Длина крыла")
-    st.plotly_chart(fig2, use_container_width=True)
-
-
-
-st.subheader("🗃 Случайные 10 строк")
-st.dataframe(df.sample(10), use_container_width=True)
 
 st.subheader("📊 Визуализация данных")
 col1, col2 = st.columns(2)
